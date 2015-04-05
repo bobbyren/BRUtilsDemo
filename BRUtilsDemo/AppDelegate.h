f9d0d8a779cfeb52a3ec139c2c54f5177ce8119d
mitrenegade <bobbyren@gmail.com>
2015-04-05 11:50:06 -0400

    Initial Commit

---
+++ BRUtilsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRUtilsDemo
+//
+//  Created by Bobby Ren on 4/5/15.
+//  Copyright (c) 2015 Bobby Ren Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
